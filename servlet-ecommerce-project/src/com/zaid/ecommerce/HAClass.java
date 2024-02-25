bbc9f47fbb18f8f215abaca691260ceefefe47bd
mohd zaid <zaidgaur7@gmail.com>
2024-02-25 18:56:51 +0530

    initial commit

---
+++ servlet-ecommerce-project/src/com/zaid/ecommerce/HAClass.java
@@ -0,0 +1,27 @@
+package com.zaid.ecommerce;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HAClass extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		ServletContext sc = getServletContext();
+		String ip = sc.getInitParameter("offer");
+		ServletConfig scc = getServletConfig();
+		String discount = scc.getInitParameter("discount");
+		writer.print(ip);
+		writer.print("<br>discount on home appliances"+discount);
+		
+	}
+
+}
